b7159d3b83432e6fa6edb07a57d4df32385a9f01
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-09-02 22:38:18 +0200

    update flake template

--- autoinsert/default-flake.nix
+++ autoinsert/default-flake.nix
@@ -2,16 +2,27 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = inputs@{
-    self, ...
-  }: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import inputs.nixpkgs { inherit system; };
-  in {
-    devShells.default = pkgs.mkShell {
-       nativeBuildInputs = with pkgs; [
-          $0
-       ];
-    };
-  });
+  outputs = inputs @ {self, ...}:
+    inputs.flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = inputs.nixpkgs.legacyPackages.\${system};
+
+      nativeBuildInputs = with pkgs; [
+      ];
+
+      buildInputs = with pkgs; [];
+    in {
+      devShells.default = pkgs.mkShell {inherit nativeBuildInputs buildInputs;};
+
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "template";
+        version = "0.0.0";
+        src = ./.;
+
+        nativeBuildInputs =
+          nativeBuildInputs
+          ++ [
+          ];
+        inherit buildInputs;
+      };
+    });
 }
