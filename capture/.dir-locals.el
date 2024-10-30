2465c7f701d686264efda6f0fd4fb32283a07aa8
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-10-30 09:53:41 +0100

    refine org capture application

--- capture/.dir-locals.el
+++ capture/.dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((org-mode . ((eval . (org-modern-mode -1))
-	      (org-descriptive-links . nil))))
+	      (org-descriptive-links . nil)
+	      (org-pretty-entities . nil))))
