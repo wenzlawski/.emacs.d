dcf0755e54b65defc4bf9915d820e3b4b6d1778c
Marc Wenzlawski <marc.wenzlawski@gmail.com>
2024-11-04 17:42:32 +0100

    Fix background color for eglot-highlight-symbol-face in modus-themes.

--- lisp/setup-eglot.el
+++ lisp/setup-eglot.el
@@ -65,7 +65,7 @@
     "Apply eglot faces"
     (set-face-attribute
      'eglot-highlight-symbol-face nil
-     :bold t :underline nil :background (modus-themes-get-color-value 'bg-hover-secondary)))
+     :bold t :underline nil :background (modus-themes-get-color-value 'bg-hover)))
 
   (with-eval-after-load 'modus-themes
     (add-hook 'modus-themes-after-load-theme-hook #'my/eglot-apply-faces)
